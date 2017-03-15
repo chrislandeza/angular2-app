b2513179cfffa9b911a49b604091d873dc798c35
Chris Landeza <CC_Landeza@yahoo.com>
2017-03-15 17:11:33 +0800

    Add todo crud on app.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import {Component} from '@angular/core';
+import {TodoDataService} from "./todo-data.service";
+import {Todo} from "./todo";
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css']
+    styleUrls: ['./app.component.css'],
+    providers: [TodoDataService]
 })
 export class AppComponent {
-    title = 'app works!';
+
+    newTodo: Todo = new Todo();
+
+    constructor(private todoDataService: TodoDataService) {
+    }
+
+    add() {
+        console.log('add the todo');
+        this.todoDataService.add(this.newTodo);
+        this.newTodo = new Todo();
+    }
+
+    toggleTodoComplete(todo) {
+        this.todoDataService.toggleTodoComplete(todo);
+    }
+
+    destroy(todo) {
+        this.todoDataService.destroy(todo.id);
+    }
+
+    get todos() {
+        return this.todoDataService.index();
+    }
 }
